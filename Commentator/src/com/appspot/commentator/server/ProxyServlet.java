1667ba55f5cf904aa401dfe898cef600354f85d4
syukatu251 <syukatu251@gmail.com>
2013-08-22 14:15:17 +0900

    HTML取得処理修正

--- Commentator/src/com/appspot/commentator/server/ProxyServlet.java
+++ Commentator/src/com/appspot/commentator/server/ProxyServlet.java
@@ -1,14 +1,26 @@
 package com.appspot.commentator.server;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 
 
@@ -25,22 +37,50 @@ public class ProxyServlet extends HttpServlet {
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
         
         for (Enumeration<?> iterator = req.getHeaderNames(); iterator.hasMoreElements();) {
+        	
             String name = (String) iterator.nextElement();
-            connection.setRequestProperty(name, req.getHeader(name));
+            if (!name.equals("Host")) {
+            	connection.setRequestProperty(name, req.getHeader(name));
+            }
         }
         
         connection.connect();
         
-/*        for (Entry<String, List<String>> headerEntry : connection.getHeaderFields().entrySet()) {
-            resp.setHeader(headerEntry.getKey(), StringUtils.join(headerEntry.getValue(), ","));
+        resp.setContentType(connection.getHeaderField("content-type"));
+        
+        Document document = null;
+        try (
+        		StringWriter stringWriter = new StringWriter();
+        		InputStream inputStream = connection.getInputStream();) {
+        	IOUtils.copy(inputStream, stringWriter);
+        	document = Jsoup.parse(stringWriter.toString());
         }
         
+        try {
+			resolve(document, req.getParameter("url"), "src");
+		} catch (URISyntaxException e1) {
+			e1.printStackTrace();
+		}
+        
+        try {
+			resolve(document, req.getParameter("url"), "href");
+		} catch (URISyntaxException e1) {
+			e1.printStackTrace();
+		}
+       
+        
         PrintWriter printWriter = resp.getWriter();
-        InputStream inputStream = connection.getInputStream();
         
-        IOUtils.copy(inputStream, printWriter);
-        
-        inputStream.close();
-        printWriter.close();*/
+        printWriter.write(document.html());
+    }
+    
+    private void resolve(Document document, String url, String attr) throws URISyntaxException {
+    	URI uri = new URI(url);
+		for (Iterator<Element> iterator = 
+        		document.getElementsByAttributeValueMatching(attr, Pattern.compile("[^^(//|http|https|#)]\\w+"))
+        		.iterator(); iterator.hasNext();) {
+        	Element element = iterator.next();
+			element.attr(attr, uri.resolve(element.attr(attr)).toString());
+		}
     }
 }
